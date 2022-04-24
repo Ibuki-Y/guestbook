90b58c2248913772cd055b16d802d4be78346ff3
Ibuki-Y <ibu.love.drum@gmail.com>
2022-04-25 01:22:14 +0900

    Add Making Decisions with a Workflow

--- src/MessageHandler/CommentMessageHandler.php
+++ src/MessageHandler/CommentMessageHandler.php
@@ -1,5 +1,5 @@
 <?php
-/* 
+/*
 CommentMessageメッセージを処理する
 マーカーインターフェース
 */
@@ -10,16 +10,28 @@ use App\Message\CommentMessage;
 use App\Repository\CommentRepository;
 use App\SpamChecker;
 use Doctrine\ORM\EntityManagerInterface;
+use Psr\Log\LoggerInterface;
 use Symfony\Component\Messenger\Handler\MessageHandlerInterface;
+use Symfony\Component\Messenger\MessageBusInterface;
+use Symfony\Component\Workflow\WorkflowInterface;
 
 class CommentMessageHandler implements MessageHandlerInterface
 {
     private $spamChecker;
     private $entityManager;
     private $commentRepository;
+    private $bus;
+    private $workflow;
+    private $logger;
 
-    public function __construct(EntityManagerInterface $entityManager, SpamChecker $spamChecker, CommentRepository $commentRepository)
-    {
+    public function __construct(
+        EntityManagerInterface $entityManager,
+        SpamChecker $spamChecker,
+        CommentRepository $commentRepository,
+        MessageBusInterface $bus,
+        WorkflowInterface $commentStateMachine,
+        LoggerInterface $logger = null
+    ) {
         $this->entityManager = $entityManager;
         $this->spamChecker = $spamChecker;
         $this->commentRepository = $commentRepository;
@@ -33,12 +45,28 @@ class CommentMessageHandler implements MessageHandlerInterface
             return;
         }
 
-        if (2 === $this->spamChecker->getSpamScore($comment, $message->getContext())) {
-            $comment->setState('spam');
-        } else {
-            $comment->setState('published');
-        }
+        /*
+        <ワークフロー>
+        メッセージ内のコメントにおいてaccept遷移が可能であればスパムチェック
+        */
+        if ($this->workflow->can($comment, 'accept')) {
+            $score = $this->spamChecker->getSpamScore($comment, $message->getContext());
+            $transition = 'accept';
+            if (2 === $score) {
+                $transition = 'reject_spam'; // 2: スパム
+            } elseif (1 === $score) {
+                $transition = 'might_be_spam'; // 1: スパムの可能性
+            }
+            $this->workflow->apply($comment, $transition); //コメント更新
+            $this->entityManager->flush(); // データベースに変更をコミット
 
-        $this->entityManager->flush();
+            // ワークフローの再遷移を許容させるためメッセージを再ディスパッチ
+            $this->bus->dispatch($message);
+        } elseif ($this->workflow->can($comment, 'publish') || $this->workflow->can($comment, 'publish_ham')) {
+            $this->workflow->apply($comment, $this->workflow->can($comment, 'publish') ? 'publish' : 'publish_ham');
+            $this->entityManager->flush();
+        } elseif ($this->logger) {
+            $this->logger->debug('Dropping comment message', ['comment' => $comment->getId(), 'state' => $comment->getState()]);
+        }
     }
 }
